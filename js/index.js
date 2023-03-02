bc266243a0a7cb3a972ef5e9a8e788e95a198298
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-03-02 09:25:24 -0700

    Add save function and a click event listener

--- js/index.js
+++ js/index.js
@@ -3,16 +3,24 @@
 // increment the count variable when the button is clicked
 // change the count in the HTML to reflect the new count
 
-let h2 = document.getElementById("count");
+let countEl = document.getElementById("count");
 
 let count = 0;
 
 function increment() {
   count += 1;
-  h2.innerText = count;
+  countEl.innerText = count;
   console.log(count);
 }
 
 let btn = document.getElementById("increment-btn");
 
 btn.addEventListener("click", increment);
+
+let saveBtn = document.getElementById("save-btn");
+
+function save() {
+  console.log(count);
+}
+
+saveBtn.addEventListener("click", save);
