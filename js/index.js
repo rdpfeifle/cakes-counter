58c2e3475f3baaa4e3ed68552c0659ba00efa756
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-03-01 23:27:48 -0700

    Add some javascript to increment the count

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-document.getElementById("count-el").innerText = 5;
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count in the HTML to reflect the new count
+
+let h2 = document.getElementById("count");
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  h2.innerText = count;
+  console.log(count);
+}
+
+let btn = document.getElementById("increment-btn");
+
+btn.addEventListener("click", increment);
